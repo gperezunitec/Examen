b37031e783abf0fa4eee08bb706d74ca338be295
Gerardo Perez <113952421+g3rar2@users.noreply.github.com>
2025-02-19 18:23:26 -0600

    added Asignatura

---
+++ Examen/Asigantura.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Examen
+{
+    internal class Asigantura : Alumno, IAsignatura
+    {
+        public double calcularNotaFinal()
+        {
+            throw new NotImplementedException();
+        }
+
+        public double calcularNotaFinal(double n1, double n2, double n3)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void imprimir()
+        {
+            throw new NotImplementedException();
+        }
+
+        public string mensajeNotaFinal(double notaFinal)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
